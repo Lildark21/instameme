2d21dd327a9fc2e8d0a934aaeed02638960a7b9a
Lildark21 <lilianorrit@gmail.com>
2024-04-02 18:41:08 +0200

    Initial commit

---
+++ composants/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" type="text/css" href="composants/header.css">
+</head>
+
+<body>
+
+
+<header class="header">
+        <a href="#" class="logo" ></a>
+        <img src="images\Instameme - Logo.png - Logo.png" alt="Instameme" width="126" height="55">
+        <nav class="navbar">
+            <input type="text" name="search" placeholder="Rechercher...">
+            <input type="submit" value="Rechercher">
+            <a href='index.php'>Acceuil</a>
+            <a href='indexconnect.php'>Connectez-vous</a>
+            <a href='#'>Contenu</a>
+        </nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'affichage.php';
+require_once 'db.php';
+
+$stmt = db()->prepare("SELECT * FROM contenus");
+$stmt->execute();
+$contenus = $stmt->fetchAll();
+?>
+
+<?php
+echo pageHeader("Instameme");
+?>
+<div class="grid grid-cols-3 gap-8">
+    <?php
+    foreach ($contenus as $contenu) {
+        echo '<div class="flex flex-col justify-center items-center space-y-2" id="' . $contenu['id'] . '">'
+            . '<img src="' . 'images/' . $contenu['chemin_image'] . '" class="h-40" />'
+            . $contenu['description']
+            . '</div>';
+    }
+    ?>
+</div>
+<?php echo pageFooter(); ?>
